fafb7714eccdcffb93fc204ff1eb9cddcdf06d93
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-03-20 12:06:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	    let r1 = date1.split('-').map(Number);
+    let r2 = date2.split('-').map(Number);
+    const dateObj1 = new Date(r1[0],r1[1]-1,r1[2]) 
+    const dateObj2 = new Date(r2[0],r2[1]-1,r2[2])
+    const miliseconds = dateObj2-dateObj1;
+    const days = miliseconds/(1000*60*60*24)
+    return days
 };
 
 // Do not change the code below.
